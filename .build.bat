bbff0851a2e6d5e811d0f1e2c397d7e30d0b8eb0
filatov04 <filka04012004@yandex.ru>
2024-01-13 06:16:49 +0300

    deleted bat

--- .build.bat
+++
@@ -1,4 +0,0 @@
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=C:/src/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build --config Release
-cmake --install build --prefix C:\spring_oop\SysCalculator
-pause
